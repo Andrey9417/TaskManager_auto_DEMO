0ccaba7086dc7a5dbcf1596a0997d7abac92698b
Andrey9417 <andreypovlat@gmail.com>
2021-05-14 14:48:50 +0300

    added 4th class

--- src/package1/Task.java
+++ src/package1/Task.java
@@ -2,6 +2,32 @@ package package1;
 
 public class Task {
 
+    private String name= "ads";
+    private int date = 24;
+    private int time = 11;
+
+    public Task(String name, int date, int time) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + date +" " + time;
+    }
     /* Тут надо определиться с полями для объекта Task
     *
     * сделать геттеры сеттеры, какие нужно
