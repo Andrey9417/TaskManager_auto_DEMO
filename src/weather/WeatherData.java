d866554c02056ac9efc461de148d29e9e0385738
VladGumeniuk <vladfutsal@gmail.com>
2021-05-17 17:47:15 +0300

    Added a weather feature.

---
+++ src/weather/WeatherData.java
@@ -0,0 +1,12 @@
+package weather;
+
+public class WeatherData {
+    Double currentTemp;
+    Double minTemp;
+    Double maxTemp;
+    Double feelsLike;
+    Double humidity;
+    Double windSpeed;
+    String country;
+    String city;
+}
